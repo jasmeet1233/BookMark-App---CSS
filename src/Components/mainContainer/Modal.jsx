6ad32d7e4d2044ad56d7343c70c1e9b977b6ebef
jasmeet1233 <jazzratra@gmail.com>
2022-02-03 12:38:48 +0530

    modal

--- src/Components/mainContainer/Modal.jsx
+++ src/Components/mainContainer/Modal.jsx
@@ -7,7 +7,9 @@ import { v4 as uuidv4 } from "uuid";
 const ModalWindow = () => {
   const dispatch = useDispatch();
   const [folderName, setFolderName] = useState("");
-  const { isModalOpen, deleteModal, deleteID } = useSelector((state) => state.bookmarkReducer);
+  const { isModalOpen, deleteModal, deleteID } = useSelector(
+    (state) => state.bookmarkReducer
+  );
   const customStyles = {
     content: {
       top: "50%",
@@ -16,16 +18,19 @@ const ModalWindow = () => {
       bottom: "auto",
       marginRight: "-50%",
       transform: "translate(-50%, -50%)",
+      borderRadius: "10px",
     },
   };
 
   const confirmDeletion = async () => {
     // const token = await JSON.parse(localStorage.getItem("bookmarkToken"));
     // const data = { folderId: id };
-    const response = await client.delete("/folder", { data: { folderId: deleteID } });
-    dispatch({ type: 'deleteHandler', payload: deleteID });
+    const response = await client.delete("/folder", {
+      data: { folderId: deleteID },
+    });
+    dispatch({ type: "deleteHandler", payload: deleteID });
     dispatch({ type: "CloseModal" });
-  }
+  };
 
   const submitHandler = async () => {
     if (!folderName) return;
@@ -52,34 +57,52 @@ const ModalWindow = () => {
 
   return (
     <Modal isOpen={isModalOpen} style={customStyles}>
-      <h3 style={{paddingBottom: '5px'}}>Folder Name</h3>
-      <input type="text" onChange={(e) => setFolderName(e.target.value)} style={{paddingLeft: '5px', marginRight: '5px'}}/>
-      <button
-        onClick={submitHandler}
-        style={{
-          border: "1px solid purple",
-          color: "purple",
-          backgroundColor: "transparent",
-          padding: "1px",
-          borderRadius: "6px",
-          marginLeft: "2px",
-        }}
-      >
-        Create Folder
-      </button>
-      <button
-        style={{
-          position: "absolute",
-          top: "2px",
-          right: "5px",
-          color: "purple",
-          backgroundColor: "transparent",
-          border: "none",
-        }}
-        onClick={() => dispatch({ type: "CloseModal" })}
-      >
-        X
-      </button>
+      <div style={{ padding: "10px", width: "300px" }}>
+        <h1 style={{ paddingBottom: "15px" }}>Folder Name</h1>
+        <input
+          type="text"
+          onChange={(e) => setFolderName(e.target.value)}
+          style={{
+            paddingLeft: "5px",
+            marginRight: "5px",
+            width: "95%",
+            borderRadius: "7px",
+            height: "30px",
+            borderStyle: "none",
+            border: "1px solid #5062b8",
+          }}
+        />
+        <button
+          onClick={submitHandler}
+          style={{
+            border: "1px solid purple",
+            color: "white",
+            backgroundColor: "#5062b8",
+            padding: "4px",
+            borderRadius: "3px",
+            marginLeft: "2px",
+            display: "block",
+            marginTop: '20px',
+            width: '150px'
+          }}
+        >
+          Create Folder
+        </button>
+        <button
+          style={{
+            position: "absolute",
+            top: "6px",
+            right: "12px",
+            color: "purple",
+            backgroundColor: "transparent",
+            border: "none",
+            fontWeight: 'bold',
+          }}
+          onClick={() => dispatch({ type: "CloseModal" })}
+        >
+          x
+        </button>
+      </div>
     </Modal>
   );
 };
